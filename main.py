4209b590dc7ee1c785ea517160c4a0e3f091d56b
AcridStingray3 <acridstingray3@gmail.com>
2020-09-12 19:52:55 +0200

    GUI can have a little thread, as a treat

--- main.py
+++ main.py
@@ -4,65 +4,75 @@ from BlackboardMeeting import BlackboardMeeting
 from MeetingJSONParser import MeetingJSONParser
 import time
 import datetime
+import threading
 from pathlib import Path
 from tkinter import W, E, Label, Button, Entry, END, Tk, StringVar
 from tkinter.filedialog import askopenfilename, sys
 
 
-def enqueue_next_week_meeting(meeting_list: list, old_meeting):
-    meeting_list.remove(old_meeting)
-    next_week = old_meeting.startTime + datetime.timedelta(days=7)
-    if type(old_meeting) is BlackboardMeeting:
-        new_meeting = BlackboardMeeting(next_week, old_meeting.duration, old_meeting.url, old_meeting.className)
-    else:
-        new_meeting = TeamsMeeting(next_week, old_meeting.duration, old_meeting.teamName, old_meeting.channelName,
-                                   old_meeting.email, old_meeting.password)
+class main:
 
-    meeting_list.append(new_meeting)
-    meeting_list.sort()
-    MeetingJSONParser.serialize_meeting_array(meeting_list)
+    stop = threading.Event()
 
+    @staticmethod
+    def enqueue_next_week_meeting(meeting_list: list, old_meeting):
+        meeting_list.remove(old_meeting)
+        next_week = old_meeting.startTime + datetime.timedelta(days=7)
+        if type(old_meeting) is BlackboardMeeting:
+            new_meeting = BlackboardMeeting(next_week, old_meeting.duration, old_meeting.url, old_meeting.className)
+        else:
+            new_meeting = TeamsMeeting(next_week, old_meeting.duration, old_meeting.teamName, old_meeting.channelName,
+                                       old_meeting.email, old_meeting.password)
 
-def main_loop(obs_path: Path, obs_profile, obs_scene_collection, obs_scene):
-    meeting_list = MeetingJSONParser.deserialize_teams()
-    meeting_list.extend(MeetingJSONParser.deserialize_blackboard())
-    meeting_list.sort()
+        meeting_list.append(new_meeting)
+        meeting_list.sort()
+        MeetingJSONParser.serialize_meeting_array(meeting_list)
 
-    while True:
-        current_meeting = meeting_list[0]
-        wait_time = current_meeting.startTime - datetime.datetime.now()
+    @staticmethod
+    def main_loop(obs_path: Path, obs_profile, obs_scene_collection, obs_scene):
+        main.stop.clear()
+        meeting_list = MeetingJSONParser.deserialize_teams()
+        meeting_list.extend(MeetingJSONParser.deserialize_blackboard())
+        meeting_list.sort()
 
-        if wait_time.total_seconds() + current_meeting.duration * 60 * 60 < 0:
-            print("The first meeting seems to be over already, skipping")
-            enqueue_next_week_meeting(meeting_list, current_meeting)
-            continue
+        while not main.stop.isSet():
+            current_meeting = meeting_list[0]
+            wait_time = current_meeting.startTime - datetime.datetime.now()
 
-        elif wait_time.total_seconds() > 0:
-            print(f"Waiting for {wait_time.total_seconds()} seconds")
-            time.sleep(wait_time.total_seconds())
+            if wait_time.total_seconds() + current_meeting.duration * 60 * 60 < 0:
+                print("The first meeting seems to be over already, skipping")
+                main.enqueue_next_week_meeting(meeting_list, current_meeting)
+                continue
 
-        try:
-            current_meeting.start_meeting()
-        except Exception as e:
-            print(f"{e} meeting start time was {current_meeting.startTime} type was {type(current_meeting)}")
-            enqueue_next_week_meeting(meeting_list, current_meeting)
-            continue  # Something bad happened with this meeting, but we can't really afford to halt execution in any one case, because then the next ones won't get joined either
-            # Usually this would be WebDriver exception, ElementNotFoundException, and other similar selenium exceptions
+            elif wait_time.total_seconds() > 0:
+                print(f"Waiting for {wait_time.total_seconds()} seconds")
+                main.stop.wait(wait_time.total_seconds())
 
-        obs = subprocess.Popen(
-            f"{obs_path} --profile \"{obs_profile}\" --startrecording --minimize-to-tray -m --collection \"{obs_scene_collection}\" --scene \"{obs_scene}\"",
-            cwd=obs_path.parent)
-        meeting_end_wait = current_meeting.startTime - datetime.datetime.now() + datetime.timedelta(hours=current_meeting.duration)  # Parameter order is a bit weird but PyCharm won't shut up about types otherwise
-        print(f"Waiting for {meeting_end_wait.total_seconds()}")
-        time.sleep(meeting_end_wait.total_seconds())
+            if main.stop.isSet():
+                return
 
-        try:
-            current_meeting.end_meeting()
-        except Exception as e:
-            print(f"{e} meeting start time was {current_meeting.startTime} type was {type(current_meeting)}")
+            try:
+                current_meeting.start_meeting()
+            except Exception as e:
+                print(f"{e} meeting start time was {current_meeting.startTime} type was {type(current_meeting)}. Exception was on starting the meeting")
+                main.enqueue_next_week_meeting(meeting_list, current_meeting)
+                continue  # Something bad happened with this meeting, but we can't really afford to halt execution in any one case, because then the next ones won't get joined either
+                # Usually this would be WebDriver exception, ElementNotFoundException, and other similar selenium exceptions
 
-        obs.kill()
-        enqueue_next_week_meeting(meeting_list, current_meeting)
+            obs = subprocess.Popen(
+                f"{obs_path} --profile \"{obs_profile}\" --startrecording --minimize-to-tray -m --collection \"{obs_scene_collection}\" --scene \"{obs_scene}\"",
+                cwd=obs_path.parent)
+            meeting_end_wait = current_meeting.startTime - datetime.datetime.now() + datetime.timedelta(hours=current_meeting.duration)  # Parameter order is a bit weird but PyCharm won't shut up about types otherwise
+            print(f"Waiting for {meeting_end_wait.total_seconds()}")
+            main.stop.wait(meeting_end_wait.total_seconds())
+
+            try:
+                current_meeting.end_meeting()
+            except Exception as e:
+                print(f"{e} meeting start time was {current_meeting.startTime} type was {type(current_meeting)}")
+
+            obs.kill()
+            main.enqueue_next_week_meeting(meeting_list, current_meeting)
 
 
 # GUI stuff follows
@@ -85,13 +95,13 @@ class GUI:
         Button(self.root, text="Browse", command=self.browsefunc, bg="#cccccc").grid(sticky=W, row=1, column=1, columnspan=1, padx=10, pady=(2, 0))
 
         Label(self.root, text="OBS Scene Collection name").grid(sticky=W + E, row=2, column=0, columnspan=2, padx=0, pady=(8, 0))
-        Entry(self.root, textvariable=self.obs_collection).grid(sticky = W + E, row = 3, columnspan = 2, padx = 10, pady = 2)
+        Entry(self.root, textvariable=self.obs_collection).grid(sticky=W + E, row=3, columnspan=2, padx=10, pady=2)
 
         Label(self.root, text="OBS Scene name").grid(sticky=W + E, row=4, column=0, columnspan=2, padx=0, pady=(8, 0))
         Entry(self.root, textvariable=self.obs_scene).grid(sticky = W + E, row = 5, columnspan = 2, padx = 10, pady = 2)
 
         Label(self.root, text="OBS Profile name").grid(sticky=W + E, row=6, column=0, columnspan=2, padx=0, pady=(8, 0))
-        Entry(self.root, textvariable=self.obs_profile).grid(sticky = W + E, row = 7, columnspan=2, padx=10, pady=2)
+        Entry(self.root, textvariable=self.obs_profile).grid(sticky=W + E, row=7, columnspan=2, padx=10, pady=2)
 
         self.start_button = Button(self.root, text="Start", command=self.start, bg="#cccccc", state="disabled")
         self.start_button.grid(sticky=W + E, row=8, column=0, columnspan=2, padx=20, pady=(20, 0))
@@ -108,15 +118,15 @@ class GUI:
         self.start_button.config(state="normal")
 
     def start(self):
-        if self.obs_path is None:
-            print("OBS path wasn't set!")
-            return
-        self.root.withdraw()
-        print("Running script, press CTRL + C to exit")
-        main_loop(self.obs_path, self.obs_profile.get(), self.obs_collection.get(), self.obs_scene.get())
+        main.stop.set()  # stop the previous one instead of having 80 threads executing
+        time.sleep(0.1)  # Race condition lol. Essentially, we don't wanna have the thread stop blocking because it was set, but then evaluate the if(set)return as not set.
+        # If my concurrency teachers saw me avoid a race condition with a sleep they'd kill me I am so sorry guys. I just really do not value the responsiveness of the button highly at all
+        main.stop.clear()
+        threading.Thread(target=main.main_loop, args=(self.obs_path, self.obs_profile.get(), self.obs_collection.get(), self.obs_scene.get())).start()
 
     @staticmethod
     def exit():
+        main.stop.set()
         sys.exit(0)
 
 
